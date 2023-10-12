36aa9c7b61a8b8f2b8360afaa19c6e2b8b7c5fb7
alissoninacio1 <alissoninacio96@gmail.com>
2023-10-12 12:38:19 -0700

    functions completed, and post working

--- src/controllers/processHeroes.js
+++ src/controllers/processHeroes.js
@@ -5,16 +5,18 @@ const {
     updateHero,
     deleteHero,
   } = require('./profiles');
+
+const connectDB = require('../db/mongo');
   
 
   // intermediate functions
-  async function processGetHero(req, res) {
+async function processGetHero(req, res) {
     const contacts = await getHeroes();
     res.json(contacts);
-  }
+}
 
   
-  async function processGetHeroById(req, res) {
+async function processGetHeroById(req, res) {
     const {id} = req.params;
     console.log('ID received:', id);
   
@@ -26,10 +28,10 @@ const {
     }
   
     res.json(contact);
-  }
+}
 
   
-  async function processCreateHeroes(req, res) {
+async function processCreateHeroes(req, res) {
     console.log('Received request body:', req.body);
   
     const heroData = req.body;
@@ -52,66 +54,66 @@ const {
       console.error('Error creating contact:', err);
       res.status(500).json({error: 'Failed to create or add a new hero.'});
     }
-  }
+}
   
 
 
-
-const heroesModel = require('../models/models');
-
 async function processUpdateHero(req, res) {
-  const { id } = req.params;
-  const updatedHeroData = req.body;
+    const { id } = req.params;
+    const updatedHeroData = req.body;
 
-  try {
-    if (!id) {
-      res.status(400).json({ error: 'Hero ID is required.' });
-      return;
-    }
+    try {
+      if (!id) {
+        res.status(400).json({ error: 'Hero ID is required.' });
+        return;
+      }
 
-    if (!updatedHeroData) {
-      res.status(400).json({ error: 'Updated hero data is required.' });
-      return;
-    }
+      if (!updatedHeroData) {
+        res.status(400).json({ error: 'Updated hero data is required.' });
+        return;
+      }
 
-    const hero = heroes.find((h) => h._id === id);
 
-    if (!hero) {
-      res.status(404).json({ error: 'Hero not found.' });
-      return;
-    }
+      const heroesCollection = await connectDB();
 
-    if ('name' in updatedHeroData) {
-      hero.name = updatedHeroData.name;
-    }
-    if ('age' in updatedHeroData) {
-      hero.age = updatedHeroData.age;
-    }
-    if ('planet' in updatedHeroData) {
-      hero.planet = updatedHeroData.planet;
-    }
-    if ('gender' in updatedHeroData) {
-      hero.gender = updatedHeroData.gender;
-    }
-    if ('alien' in updatedHeroData) {
-      hero.alien = updatedHeroData.alien;
-    }
-    if ('tendency' in updatedHeroData) {
-      hero.tendency = updatedHeroData.tendency;
-    }
-    if ('editorial' in updatedHeroData) {
-      hero.editorial = updatedHeroData.editorial;
-    }
+      const ObjectId = require('mongodb').ObjectId;
+      const objectId = new ObjectId(id);
 
-    res.status(204).json({ message: 'Hero updated successfully.' });
-  } catch (err) {
-    console.error('Error updating hero:', err);
-    res.status(500).json({ error: 'Failed to update hero.' });
-  }
+      const hero = await heroesCollection.findOne({ _id: objectId });
+
+      if (!hero) {
+        res.status(404).json({ error: 'Hero not found.' });
+        return;
+      }
+
+      const fieldsToUpdate = ['name', 'age', 'planet', 'gender', 'alien', 'tendency', 'editorial'];
+      const updateQuery = {};
+
+      for (const field of fieldsToUpdate) {
+        if (field in updatedHeroData) {
+          updateQuery[field] = updatedHeroData[field];
+
+          //see which fields were updated
+          console.log(`${hero._id} - ${field} updated successfully`)
+        }
+      }
+
+      const result = await heroesCollection.updateOne({ _id: objectId }, { $set: updateQuery });
+
+      if (result.modifiedCount === 0) {
+        res.status(500).json({ error: 'Failed to update hero.' });
+      } else {
+        res.status(204).json({ message: 'Hero updated successfully.' });
+      }
+
+
+    } catch (err) {
+      console.error('Error updating hero:', err);
+      res.status(500).json({ error: 'Failed to update hero.' });
+    }
 }
 
   
-
   
   async function processDeleteHero(req, res) {
     const {id} = req.params;
@@ -130,6 +132,8 @@ async function processUpdateHero(req, res) {
     }
   }
   
+
+
   module.exports = {
     processGetHero,
     processGetHeroById,
