95a83ce860f6e224b9a74be564cf39d28e7846c4
alissoninacio1 <alissoninacio96@gmail.com>
2023-10-26 19:08:04 -0700

    oauth logic added

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const port = 5000;
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json'); 
 const cors = require('cors');
+const passport = require('./auth');  //oauth
 
 app.use(cors());
 
@@ -13,6 +14,8 @@ const heroesRouter = require('./routes/index');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
+
+app.use(passport.initialize());  //oauth
 
 app.use('/', heroesRouter);
 app.use('/heroes', heroesRouter);
---
+++ src/auth.js
@@ -0,0 +1,47 @@
+// auth.js
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const dotenv = require('dotenv');
+
+dotenv.config(); 
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID, 
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+    callbackURL: process.env.GOOGLE_CALLBACK_URL 
+},
+function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+}));
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+
+passport.initLoginRoute = (app) => {
+    app.get('/login', (req, res) => {
+        passport.authenticate('google', {
+            scope: [
+                'https://www.googleapis.com/auth/userinfo.profile', 
+                'https://www.googleapis.com/auth/userinfo.email'
+            ]
+        })(req, res);
+    });
+}
+
+
+passport.initCallbackRoute = (app) => {
+    app.get('/auth/google/callback',
+        passport.authenticate('google', { failureRedirect: '/login' }),
+        function(req, res) {
+            res.redirect('/profile'); 
+        }
+    );
+}
+
+module.exports = passport;
